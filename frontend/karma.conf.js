b30794c71fea9a3acd055cecee16d8a32d958225
jessiexem <jessiexem@hotmail.com>
2022-11-11 13:15:52 +0800

    change app name to socialit

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/homework_frontend'),
+      dir: require('path').join(__dirname, './coverage/socialit'),
       subdir: '.',
       reporters: [
         { type: 'html' },
